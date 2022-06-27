faf47f43ac83e84124634e350dbd5fc11257458a
adiosthatokay <48399988+adiosthatokay@users.noreply.github.com>
2022-06-27 20:41:33 +0900

    追加されるアクセサリをSwitchからLightbulbに変更
    扱うことのできる信号をmax, high, low, offの4パターンに増やした

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# homebridge-nremo
-[![npm package](https://nodei.co/npm/homebridge-nremo.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/homebridge-nremo/)
-
-Nature RemoをSiriで操作できるようにする(on/offのみ)
+# homebridge-nremo-lightbulb
+[nillpo/homebridge-nremo](https://github.com/nillpo/homebridge-nremo)からフォーク
+                          
+Nature Remoで任意の赤外線信号を送信できるようにするプラグイン  
+電球（Lightbulb）型のアクセサリを追加  
+`max`, `high`, `low`, `off` の4パターンの信号を扱うことができる
 
 # Configuration
 ```JSON
@@ -16,55 +18,27 @@ Nature RemoをSiriで操作できるようにする(on/offのみ)
       "timeout": 2000,
       "retry_interval": 500,
       "retry": 3,
-      "on": { "format": "us", "freq": 38, "data": [] },
-      "off": { "format": "us", "freq": 38, "data": [] },
-      "command_order": [
-        { "on": ["on"] },
-        { "off": ["off"] }
-      ]
-    },
-    {
-      "name": "Lamp A",
-      "accessory": "NatureRemo",
-      "host": "Remo-XXXX.local",
-      "path": "/messages",
-      "delayBefore": 1000,
-      "delayAfter": 1000,
-      "timeout": 2000,
-      "retry_interval": 500,
-      "retry": 3,
-      "on": { "format": "us", "freq": 38, "data": [] },
-      "off": { "format": "us", "freq": 38, "data": [] },
-      "command_order": [
-        { "on": ["on", "off", "on", "off"] },
-        { "off": ["on", "off"] }
-      ]
-    },
-    {
-      "name": "Lamp B",
-      "accessory": "NatureRemo",
-      "host": "Remo-XXXX.local",
-      "path": "/messages",
-      "delayBefore": 500,
-      "delayAfter": 800,
-      "timeout": 2000,
-      "retry_interval": 500,
-      "retry": 3,
-      "s_on": { "format": "us", "freq": 38, "data": [] },
-      "s_middle": { "format": "us", "freq": 38, "data": [] },
+      "s_max": { "format": "us", "freq": 38, "data": [] },
+      "s_high": { "format": "us", "freq": 38, "data": [] },
+      "s_low": { "format": "us", "freq": 38, "data": [] },
       "s_off": { "format": "us", "freq": 38, "data": [] },
       "command_order": [
-        { "on": ["s_on", "s_middle", "s_off"] },
+        { "max": ["s_max", "s_high", "s_low"] },
+        { "high": ["s_high"] },
+        { "low": ["s_low"] },
         { "off": ["s_off"] }
       ]
-    }
+    },
   ],
 ```
-on/off時にcommand_orderのon/offごとの[]で指定したコマンドをdelayAfterで指定した時間ごとに送信する
-* delayBefore: コマンド送信前に指定時間分だけ待つ(Aアクセサリ実行後に実行後Bアクセサリ実行などのシーン利用時に便利)
-* delayAfter: on/off実行時に[]内のコマンドを指定時間ごとに送信できる(同時に送るとNature Remoが反応しない)
-* retry: リクエストが失敗したときに再試行する回数
-* retry_interval: 再試行するまでの時間
-* timeout: リクエストがタイムアウトするまでの時間
+アクセサリの明るさを100％にすると`max`の信号を、51％～99％で`high`の信号を、1％～50％で`low`の信号を、0％で`off`の信号を送信する  
 
-**Nature Remoにアクセスがしばらくないと1回目のリクエスト時にタイムアウトするのでtimeoutで短めに設定するのがよさそう**
+**以下フォーク元と同じ**  
+>on/off時にcommand_orderのon/offごとの[]で指定したコマンドをdelayAfterで指定した時間ごとに送信する
+>* delayBefore: コマンド送信前に指定時間分だけ待つ(Aアクセサリ実行後に実行後Bアクセサリ実行などのシーン利用時に便利)
+>* delayAfter: on/off実行時に[]内のコマンドを指定時間ごとに送信できる(同時に送るとNature Remoが反応しない)
+>* retry: リクエストが失敗したときに再試行する回数
+>* retry_interval: 再試行するまでの時間
+>* timeout: リクエストがタイムアウトするまでの時間
+>
+>**Nature Remoにアクセスがしばらくないと1回目のリクエスト時にタイムアウトするのでtimeoutで短めに設定するのがよさそう**
--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const http = require('http');
 const PromiseRetry = require('./lib/promiseretry');
+const semaphore = require('await-semaphore');
+const mutex = new semaphore.Mutex();
 let Service;
 let Characteristic;
 
 module.exports = homebridge => {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-nremo', 'NatureRemo', RemoAccessory);
+  homebridge.registerAccessory('homebridge-nremo-lightbulb', 'NatureRemoLightBulb', RemoAccessory);
 };
 
 class RemoAccessory {
   constructor(log, config) {
     this.log = log;
     this.name = config['name'];
-    this.switchService = new Service.Switch(this.name);
+    this.lightBulb = new Service.Lightbulb(this.name);
     this.informationService = new Service.AccessoryInformation();
     this.config = config;
-    this.order_on = config.command_order[0];
-    this.order_off = config.command_order[1];
+    this.order_max = config.command_order[0];
+    this.order_high = config.command_order[1];
+    this.order_low = config.command_order[2];
+    this.order_off = config.command_order[3];
   }
 
   request(command, delayAfter = 0, timeout = 2000) {
@@ -59,22 +63,71 @@ class RemoAccessory {
     });
   }
 
-  async setState(input_switch_state, next) {
-    this.log(`Swtich: ${input_switch_state}`);
+  async setState(input_bulb_state, callback) {
+    this.log(`Swtich: ${input_bulb_state}`);
+    if (input_bulb_state && this.brightness === 0) {
+      await this.setBrightness(100, callback);
+    } else if (input_bulb_state) {
+      await this.setBrightness(this.brightness, callback);
+    } else {
+      await this.sendIr('off', callback);
+    }
+  }
+
+  async getBrightness(callback) {
+    this.log("get brightness: " + this.brightness);
+    callback(null, this.brightness);
+  }
+
+  async setBrightness(value, callback) {
+    this.log("set brightness: " + value);
+    this.brightness = value;
 
     let command_order;
-    if (input_switch_state) {
-      command_order = 'on';
+    if (value === 100) {
+      command_order = 'max';
+    } else if (50 < value && value < 100) {
+      command_order = 'high';
+    } else if (0 < value && value <= 50) {
+      command_order = 'low';
     } else {
       command_order = 'off';
     }
+    await this.sendIr(command_order, callback);
+  }
+
+  getServices() {
+    this.log(`start homebridge Server ${this.name}`);
+
+    this.informationService
+      .setCharacteristic(Characteristic.Manufacturer, 'Nature')
+      .setCharacteristic(Characteristic.Model, 'Remo')
+      .setCharacteristic(Characteristic.SerialNumber, '123-457-000');
+
+    this.lightBulb
+      .getCharacteristic(Characteristic.On)
+      .on('set', this.setState.bind(this));
+    this.lightBulb.getCharacteristic(Characteristic.Brightness)
+      .on('get', this.getBrightness.bind(this))
+      .on('set', this.setBrightness.bind(this));
+    return [this.informationService, this.lightBulb];
+  }
+
+  identify(callback) {
+    this.log(`called ${this.config['name']}`);
+    callback();
+  }
+
+  async sendIr(command_order, next) {
     const pre = 'order_' + command_order;
 
     await new Promise(resolve =>
       setTimeout(resolve, this.config.delayBefore || 0)
     );
 
+    let release;
     try {
+      release = await mutex.acquire();
       for (let i = 0; i < this[pre][command_order].length; i++) {
         const promise = new PromiseRetry(
           this.config.retry,
@@ -89,30 +142,19 @@ class RemoAccessory {
           );
         });
         this.log(`${this[pre][command_order][i]}: ${response.status}`);
+        if (command_order === 'off') {
+          this.state = false;
+        } else {
+          this.state = true;
+        }
       }
       next();
     } catch (error) {
       this.log(error.message);
       next(error);
+    } finally {
+      release();
     }
   }
 
-  getServices() {
-    this.log(`start homebridge Server ${this.name}`);
-
-    this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, 'Nature')
-      .setCharacteristic(Characteristic.Model, 'Remo')
-      .setCharacteristic(Characteristic.SerialNumber, '123-457-000');
-
-    this.switchService
-      .getCharacteristic(Characteristic.On)
-      .on('set', this.setState.bind(this));
-    return [this.informationService, this.switchService];
-  }
-
-  identify(callback) {
-    this.log(`called ${this.config['name']}`);
-    callback();
-  }
 }
